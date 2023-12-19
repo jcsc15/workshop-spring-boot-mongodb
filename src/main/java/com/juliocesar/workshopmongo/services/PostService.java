1d9b2d893600880c907311a9d94b4577e314e590
Julio Cesar <kucip2234@gmail.com>
2023-12-19 07:55:41 -0300

    Simple query with query methods

--- src/main/java/com/juliocesar/workshopmongo/services/PostService.java
+++ src/main/java/com/juliocesar/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.juliocesar.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
-	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 
 }
